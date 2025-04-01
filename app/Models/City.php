93015ba79ffcfa784ea36085cd58ea00c66a9744
Kakajan77 <kakajanedepliyew023@gmail.com>
2025-04-01 12:03:03 +0500

    first commit

---
+++ app/Models/City.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class City extends Model
+{
+    protected $guarded = [
+        'id',
+    ];
+
+    public $timestamps = false;
+
+    public function restaurant(): HasMany
+    {
+        return $this->hasMany(Restaurant::class);
+    }
+
+}
